4745964dc3e7a6fe230222df82a025c3e66a25ff
Md Rohidul Islam <rohidul209@gmail.com>
2022-07-27 17:08:20 +0600

    init turbo repo with a nextjs and strapi app

---
+++ apps/blog/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const HomePage = () => {
+  return (
+    <div>
+      <h2 className="text-5xl text-red-500">HomePage</h2>
+    </div>
+  );
+};
+
+export default HomePage;
